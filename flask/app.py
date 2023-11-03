2b441fb4dac54d39742bcfe4e0bf0b162ae059e3
Nab <nhussainjee@gmail.com>
2023-11-03 18:49:10 +0500

    initial commit for RE website 01_one_page

---
+++ flask/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+
+app = Flask(__name__)
+print(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
